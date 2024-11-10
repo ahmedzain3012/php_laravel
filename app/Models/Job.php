5d1c2833b52e1a3fb81037c7367de310e2eafcc0
Ahmed.Zain <arz3012@gmail.com>
2024-11-11 01:39:32 +0200

    lesson16

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -9,7 +9,10 @@ class Job extends Model
 {
   use HasFactory;
   protected $table = "job_listings";
-  protected $fillable = ['title', 'salary'];
+  
+  //protected $fillable = ['title', 'salary','employer_id'];
+  
+  protected $guarded = [];
 
   public function employer()
   {
--- routes/web.php
+++ routes/web.php
@@ -16,18 +16,32 @@ Route::get('/contact', function () {
 });
 
 Route::get('/jobs', function () {
-    $job = Job::with('employer')->cursorPaginate(3);
-    return view('jobs', [
+$job = Job::with('employer')->latest()->cursorPaginate(3);
+    return view('jobs/index', [
         'greeting' => 'Wellcome',
         'jobs' => $job
     ]);
 });
 
+Route::get('/jobs/create', function () {
+    return view('jobs/create');
+    //dd('hoiiiiii');
+});
+
+Route::post('/jobs', function () {
+    //return view('jobs/create');
+    //dd(request()->all());
+    // validation
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1
+    ]);
+    return redirect('/jobs');
+});
+
 Route::get('/jobs/{id}', function ($id) {
-
-
     $job = Job::find($id);
     // dd($job);
-
-    return view('job', ['job' => $job]);
+    return view('jobs/show', ['job' => $job]);
 });
